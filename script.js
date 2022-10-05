7e5c72e7d12ee5cf7ad9858b4ddb9020bde68acd
joao.sipauba <joao.sipauba@ceuma.br>
2022-10-05 11:18:59 -0300

    desafio ceuma

---
+++ script.js
@@ -0,0 +1,12 @@
+function consultaImagens() {
+    let searchbox = document.querySelector('#searchbox').value;
+
+     
+    
+}
+
+let url = 'http://34.176.250.192:8080/swagger-ui/index.html#/'
+
+fetch(url).then(function(response){ 
+    console.log(response);
+});
